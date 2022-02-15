07e227b3820d350efae041b001492b97cfe6cbbf
CrisKerps <95253021+CrisKerps@users.noreply.github.com>
2022-02-15 09:48:50 -0300

    PortfolioWebPage(React)

---
+++ Portfolio web page/client/src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from './Profile/profile'
+import Footer from './Footer/footer'
+import './Home.css'
+
+export default function 
+() {
+  return (
+    <div className='home-container'>
+        <Profile/>
+        <Footer/>
+    </div>
+  )
+}
